b06c78b1ae16fa9511ca65fbd1f64a6048cf4b9c
Payton Gunnell <paytongunnell@gmail.com>
2025-10-24 09:38:45 -0700

    initial commit.

---
+++ random_password_generator.py
@@ -0,0 +1,58 @@
+import random
+import string
+
+list_of_special_characters = ['!', '@', '#', '%', '&', '-', '+', '=']
+make_password = "y"
+
+while make_password == "y":
+    password_list = []
+    number_of_characters = int(input("How many characters?(Must be at least 4): "))
+
+    if number_of_characters < 4:
+        while number_of_characters < 4:
+            number_of_characters = int(input("Please enter a valid amout of characters (Must be at least 4): "))
+
+    def random_int():
+        return str(random.randint(0, 9))
+    def random_lowercase():
+        return str(random.choice(string.ascii_lowercase))
+    def random_uppercase():
+        return str(random.choice(string.ascii_uppercase))
+    def random_special_character():
+        return str(random.choice(list_of_special_characters))
+    def random_character(index):
+        random_function_index = 1
+        if index <= 4:
+            random_function_index = index
+        else:
+            random_function_index = index % 4 or 4
+        
+        match random_function_index:
+            case 1:
+                return random_int()
+            case 2: 
+                return random_lowercase()
+            case 3:
+                return random_uppercase()
+            case 4: 
+                return random_special_character()
+            case _:
+                print("Error line 33")
+                return random_lowercase()
+        
+
+    def create_password(num_characters):
+        password_list = [random_int(), random_lowercase(), random_uppercase(), random_special_character()]
+        while int(len(password_list)) < num_characters:
+            password_list.append(random_character(int(len(password_list))))
+        random.shuffle(password_list)
+        return "".join(password_list)
+
+    password = create_password(number_of_characters)
+
+    
+
+    print(f"Your new password: {password}")
+
+    make_password = input("Make another password? y/n: ")
+
